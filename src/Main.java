121c473addde85eb88d1c578898edd178c434e11
wbenica <43890908+wbenica@users.noreply.github.com>
2019-02-22 01:30:23 -0800

    Added database controller class, jdbc connection. Time spent - 1 hr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle ( "Bicycle Inventory" );
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
